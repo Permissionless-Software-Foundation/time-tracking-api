792cfcb2a8ac60afe6c6861604d8d39a4c534dd5
Chris Troutner <chris.troutner@gmail.com>
2019-05-19 18:23:23 -0700

    feat(projects): Adding project model and API endpoints

---
+++ src/modules/projects/controller.js
@@ -0,0 +1,238 @@
+const Project = require('../../models/projects')
+
+/**
+ * @api {post} /projects Create a new project
+ * @apiPermission
+ * @apiVersion 1.0.0
+ * @apiName CreateProject
+ * @apiGroup Projects
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X POST -d '{ "project": { "projectname": "johndoe", "password": "secretpasas" } }' localhost:5000/projects
+ *
+ * @apiParam {Object} project          Project object (required)
+ * @apiParam {String} project.projectname Projectname.
+ * @apiParam {String} project.password Password.
+ *
+ * @apiSuccess {Object}   projects           Project object
+ * @apiSuccess {ObjectId} projects._id       Project id
+ * @apiSuccess {String}   project.type       Project type (admin or project)
+ * @apiSuccess {String}   projects.name      Project name
+ * @apiSuccess {String}   projects.projectname  Project projectname
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "project": {
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "John Doe"
+ *          "projectname": "johndoe"
+ *       }
+ *     }
+ *
+ * @apiError UnprocessableEntity Missing required parameters
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 422 Unprocessable Entity
+ *     {
+ *       "status": 422,
+ *       "error": "Unprocessable Entity"
+ *     }
+ */
+async function createProject (ctx) {
+  const project = new Project(ctx.request.body.project)
+
+  try {
+    await project.save()
+  } catch (err) {
+    ctx.throw(422, err.message)
+  }
+
+  ctx.body = {
+    success: true
+  }
+}
+
+/**
+ * @api {get} /projects Get all projects
+ * @apiPermission project
+ * @apiVersion 1.0.0
+ * @apiName GetProjects
+ * @apiGroup Projects
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X GET localhost:5000/projects
+ *
+ * @apiSuccess {Object[]} projects           Array of project objects
+ * @apiSuccess {ObjectId} projects._id       Project id
+ * @apiSuccess {String}   project.type       Project type (admin or project)
+ * @apiSuccess {String}   projects.name      Project name
+ * @apiSuccess {String}   projects.projectname  Project projectname
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "projects": [{
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "John Doe"
+ *          "projectname": "johndoe"
+ *       }]
+ *     }
+ *
+ * @apiUse TokenError
+ */
+async function getProjects (ctx) {
+  const projects = await Project.find({})
+  ctx.body = { projects }
+}
+
+/**
+ * @api {get} /projects/:id Get project by id
+ * @apiPermission project
+ * @apiVersion 1.0.0
+ * @apiName GetProject
+ * @apiGroup Projects
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X GET localhost:5000/projects/56bd1da600a526986cf65c80
+ *
+ * @apiSuccess {Object}   projects           Project object
+ * @apiSuccess {ObjectId} projects._id       Project id
+ * @apiSuccess {String}   project.type       Project type (admin or project)
+ * @apiSuccess {String}   projects.name      Project name
+ * @apiSuccess {String}   projects.projectname  Project projectname
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "project": {
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "John Doe"
+ *          "projectname": "johndoe"
+ *       }
+ *     }
+ *
+ * @apiUse TokenError
+ */
+async function getProject (ctx, next) {
+  try {
+    const project = await Project.findById(ctx.params.id)
+    if (!project) {
+      ctx.throw(404)
+    }
+
+    ctx.body = {
+      project
+    }
+  } catch (err) {
+    if (err === 404 || err.name === 'CastError') {
+      ctx.throw(404)
+    }
+
+    ctx.throw(500)
+  }
+
+  if (next) {
+    return next()
+  }
+}
+
+/**
+ * @api {put} /projects/:id Update a project
+ * @apiPermission
+ * @apiVersion 1.0.0
+ * @apiName UpdateProject
+ * @apiGroup Projects
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X PUT -d '{ "project": { "name": "Cool new Name" } }' localhost:5000/projects/56bd1da600a526986cf65c80
+ *
+ * @apiParam {Object} project          Project object (required)
+ * @apiParam {String} project.name     Name.
+ * @apiParam {String} project.projectname Projectname.
+ *
+ * @apiSuccess {Object}   projects           Project object
+ * @apiSuccess {ObjectId} projects._id       Project id
+ * @apiSuccess {String}   project.type      Project type (admin or project)
+ * @apiSuccess {String}   projects.name      Updated name
+ * @apiSuccess {String}   projects.projectname  Updated projectname
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "project": {
+ *          "_id": "56bd1da600a526986cf65c80"
+ *          "name": "Cool new name"
+ *          "projectname": "johndoe"
+ *       }
+ *     }
+ *
+ * @apiError UnprocessableEntity Missing required parameters
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 422 Unprocessable Entity
+ *     {
+ *       "status": 422,
+ *       "error": "Unprocessable Entity"
+ *     }
+ *
+ * @apiUse TokenError
+ */
+async function updateProject (ctx) {
+  const project = ctx.body.project
+
+  Object.assign(project, ctx.request.body.project)
+
+  console.log(`Current user: ${JSON.stringify(ctx.state.user, null, 2)}`)
+
+  // Unless the calling project is an admin, they can not change the project type.
+  // if (projectType !== 'admin') {
+  //  project.type = projectType
+  // }
+
+  // await project.save()
+
+  ctx.body = {
+    project
+  }
+}
+
+/**
+ * @api {delete} /projects/:id Delete a project
+ * @apiPermission
+ * @apiVersion 1.0.0
+ * @apiName DeleteProject
+ * @apiGroup Projects
+ *
+ * @apiExample Example usage:
+ * curl -H "Content-Type: application/json" -X DELETE localhost:5000/projects/56bd1da600a526986cf65c80
+ *
+ * @apiSuccess {StatusCode} 200
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "success": true
+ *     }
+ *
+ * @apiUse TokenError
+ */
+
+async function deleteProject (ctx) {
+  const project = ctx.body.project
+
+  await project.remove()
+
+  ctx.status = 200
+  ctx.body = {
+    success: true
+  }
+}
+
+module.exports = {
+  createProject,
+  getProjects,
+  getProject,
+  updateProject,
+  deleteProject
+}
---
+++ test/a03-projects.spec.js
@@ -0,0 +1,494 @@
+const testUtils = require('./utils')
+const rp = require('request-promise')
+const assert = require('chai').assert
+const config = require('../config')
+
+const util = require('util')
+util.inspect.defaultOptions = { depth: 1 }
+
+const LOCALHOST = `http://localhost:${config.port}`
+
+const context = {}
+
+describe('Projects', () => {
+  before(async () => {
+    // Create a second test user.
+    const userObj = {
+      username: 'test',
+      password: 'pass'
+    }
+    const testUser = await testUtils.createUser(userObj)
+
+    context.user = testUser.user
+    context.token = testUser.tokend
+  })
+
+  describe('POST /projects', () => {
+    it('should reject project creation if no JWT provided', async () => {
+      try {
+        const options = {
+          method: 'POST',
+          uri: `${LOCALHOST}/projects`,
+          resolveWithFullResponse: true,
+          json: true,
+          body: {
+            title: 'test project'
+          }
+        }
+
+        let result = await rp(options)
+        console.log(`result stringified: ${JSON.stringify(result, null, 2)}`)
+
+        assert(false, 'Unexpected result')
+      } catch (err) {
+        assert(err.statusCode === 401, 'Error code 401 expected.')
+      }
+    })
+    /*
+    it('should sign up', async () => {
+      try {
+        const options = {
+          method: 'POST',
+          uri: `${LOCALHOST}/users`,
+          resolveWithFullResponse: true,
+          json: true,
+          body: {
+            user: { username: 'supercoolname', password: 'supersecretpassword' }
+          }
+        }
+
+        let result = await rp(options)
+        // console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+        context.user = result.body.user
+        context.token = result.body.token
+
+        assert(result.statusCode === 200, 'Status Code 200 expected.')
+        assert(result.body.user.username === 'supercoolname', 'Username of test expected')
+        assert(result.body.user.password === undefined, 'Password expected to be omited')
+        assert.property(result.body, 'token', 'Token property exists.')
+      } catch (err) {
+        console.log(
+          'Error authenticating test user: ' + JSON.stringify(err, null, 2)
+        )
+        throw err
+      }
+    })
+*/
+  })
+/*
+  describe('GET /users', () => {
+    it('should not fetch users if the authorization header is missing', async () => {
+      try {
+        const options = {
+          method: 'GET',
+          uri: `${LOCALHOST}/users`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Accept: 'application/json'
+          }
+        }
+
+        await rp(options)
+
+        assert.equal(true, false, 'Unexpected behavior')
+      } catch (err) {
+        assert.equal(err.statusCode, 401)
+      }
+    })
+
+    it('should not fetch users if the authorization header is missing the scheme', async () => {
+      try {
+        const options = {
+          method: 'GET',
+          uri: `${LOCALHOST}/users`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Accept: 'application/json',
+            Authorization: '1'
+          }
+        }
+
+        await rp(options)
+        assert.equal(true, false, 'Unexpected behavior')
+      } catch (err) {
+        assert.equal(err.statusCode, 401)
+      }
+    })
+
+    it('should not fetch users if the authorization header has invalid scheme', async () => {
+      const { token } = context
+      try {
+        const options = {
+          method: 'GET',
+          uri: `${LOCALHOST}/users`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Unknown ${token}`
+          }
+        }
+
+        await rp(options)
+        assert.equal(true, false, 'Unexpected behavior')
+      } catch (err) {
+        assert.equal(err.statusCode, 401)
+      }
+    })
+
+    it('should not fetch users if token is invalid', async () => {
+      try {
+        const options = {
+          method: 'GET',
+          uri: `${LOCALHOST}/users`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer 1`
+          }
+        }
+
+        await rp(options)
+        assert.equal(true, false, 'Unexpected behavior')
+      } catch (err) {
+        assert.equal(err.statusCode, 401)
+      }
+    })
+
+    it('should fetch all users', async () => {
+      const { token } = context
+
+      const options = {
+        method: 'GET',
+        uri: `${LOCALHOST}/users`,
+        resolveWithFullResponse: true,
+        json: true,
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      const result = await rp(options)
+      const users = result.body.users
+      // console.log(`users: ${util.inspect(users)}`)
+
+      assert.hasAnyKeys(users[0], ['type', '_id', 'username'])
+      assert.isNumber(users.length)
+    })
+  })
+
+  describe('GET /users/:id', () => {
+    it('should not fetch user if token is invalid', async () => {
+      try {
+        const options = {
+          method: 'GET',
+          uri: `${LOCALHOST}/users/1`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer 1`
+          }
+        }
+
+        await rp(options)
+        assert.equal(true, false, 'Unexpected behavior')
+      } catch (err) {
+        assert.equal(err.statusCode, 401)
+      }
+    })
+
+    it("should throw 404 if user doesn't exist", async () => {
+      const { token } = context
+
+      try {
+        const options = {
+          method: 'GET',
+          uri: `${LOCALHOST}/users/1`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+          }
+        }
+
+        await rp(options)
+        assert.equal(true, false, 'Unexpected behavior')
+      } catch (err) {
+        assert.equal(err.statusCode, 404)
+      }
+    })
+
+    it('should fetch user', async () => {
+      const {
+        user: { _id },
+        token
+      } = context
+
+      const options = {
+        method: 'GET',
+        uri: `${LOCALHOST}/users/${_id}`,
+        resolveWithFullResponse: true,
+        json: true,
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      const result = await rp(options)
+      const user = result.body.user
+      // console.log(`user: ${util.inspect(user)}`)
+
+      assert.hasAnyKeys(user, ['type', '_id', 'username'])
+      assert.equal(user._id, _id)
+      assert.notProperty(
+        user,
+        'password',
+        'Password property should not be returned'
+      )
+    })
+  })
+
+  describe('PUT /users/:id', () => {
+    it('should not update user if token is invalid', async () => {
+      try {
+        const options = {
+          method: 'PUT',
+          uri: `${LOCALHOST}/users/1`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer 1`
+          }
+        }
+
+        await rp(options)
+        assert.equal(true, false, 'Unexpected behavior')
+      } catch (err) {
+        assert.equal(err.statusCode, 401)
+      }
+    })
+
+    it('should throw 401 if non-admin updating other user', async () => {
+      const { token } = context
+
+      try {
+        const options = {
+          method: 'PUT',
+          uri: `${LOCALHOST}/users/1`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+          }
+        }
+
+        await rp(options)
+        assert.equal(true, false, 'Unexpected behavior')
+      } catch (err) {
+        assert.equal(err.statusCode, 401)
+      }
+    })
+
+    it('should update user', async () => {
+      const {
+        user: { _id },
+        token
+      } = context
+
+      const options = {
+        method: 'PUT',
+        uri: `${LOCALHOST}/users/${_id}`,
+        resolveWithFullResponse: true,
+        json: true,
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: {
+          user: { username: 'updatedcoolname' }
+        }
+      }
+
+      const result = await rp(options)
+      const user = result.body.user
+      // console.log(`user: ${util.inspect(user)}`)
+
+      assert.hasAnyKeys(user, ['type', '_id', 'username'])
+      assert.equal(user._id, _id)
+      assert.notProperty(
+        user,
+        'password',
+        'Password property should not be returned'
+      )
+      assert.equal(user.username, 'updatedcoolname')
+    })
+
+    it('should not be able to update user type', async () => {
+      try {
+        const options = {
+          method: 'PUT',
+          uri: `${LOCALHOST}/users/${context.user._id.toString()}`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Authorization: `Bearer ${context.token}`
+          },
+          body: {
+            user: {
+              name: 'new name',
+              type: 'test'
+            }
+          }
+        }
+
+        let result = await rp(options)
+
+        // console.log(`Users: ${JSON.stringify(result, null, 2)}`)
+
+        assert(result.statusCode === 200, 'Status Code 200 expected.')
+        assert(result.body.user.type === 'user', 'Type should be unchanged.')
+      } catch (err) {
+        console.error('Error: ', err)
+        console.log('Error stringified: ' + JSON.stringify(err, null, 2))
+        throw err
+      }
+    })
+
+    it('should not be able to update other user', async () => {
+      try {
+        const options = {
+          method: 'PUT',
+          uri: `${LOCALHOST}/users/${context.user2._id.toString()}`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Authorization: `Bearer ${context.token}`
+          },
+          body: {
+            user: {
+              name: 'This should not work'
+            }
+          }
+        }
+
+        let result = await rp(options)
+
+        console.log(`result stringified: ${JSON.stringify(result, null, 2)}`)
+        assert(false, 'Unexpected result')
+      } catch (err) {
+        if (err.statusCode === 401) {
+          assert(err.statusCode === 401, 'Error code 401 expected.')
+        } else {
+          console.error('Error: ', err)
+          console.log('Error stringified: ' + JSON.stringify(err, null, 2))
+          throw err
+        }
+      }
+    })
+  })
+
+  describe('DELETE /users/:id', () => {
+    it('should not delete user if token is invalid', async () => {
+      try {
+        const options = {
+          method: 'DELETE',
+          uri: `${LOCALHOST}/users/1`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer 1`
+          }
+        }
+
+        await rp(options)
+        assert.equal(true, false, 'Unexpected behavior')
+      } catch (err) {
+        assert.equal(err.statusCode, 401)
+      }
+    })
+
+    it('should throw 401 if deleting other user', async () => {
+      const { token } = context
+
+      try {
+        const options = {
+          method: 'DELETE',
+          uri: `${LOCALHOST}/users/1`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+          }
+        }
+
+        await rp(options)
+        assert.equal(true, false, 'Unexpected behavior')
+      } catch (err) {
+        assert.equal(err.statusCode, 401)
+      }
+    })
+
+    it('should not be able to delete other users unless admin', async () => {
+      try {
+        const options = {
+          method: 'DELETE',
+          uri: `${LOCALHOST}/users/${context.user2._id.toString()}`,
+          resolveWithFullResponse: true,
+          json: true,
+          headers: {
+            Authorization: `Bearer ${context.token}`
+          }
+        }
+
+        let result = await rp(options)
+
+        console.log(`result stringified: ${JSON.stringify(result, null, 2)}`)
+        assert(false, 'Unexpected result')
+      } catch (err) {
+        if (err.statusCode === 401) {
+          assert(err.statusCode === 401, 'Error code 401 expected.')
+        } else {
+          console.error('Error: ', err)
+          console.log('Error stringified: ' + JSON.stringify(err, null, 2))
+          throw err
+        }
+      }
+    })
+
+    it('should delete user', async () => {
+      const {
+        user: { _id },
+        token
+      } = context
+
+      const options = {
+        method: 'DELETE',
+        uri: `${LOCALHOST}/users/${_id}`,
+        resolveWithFullResponse: true,
+        json: true,
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      const result = await rp(options)
+      // console.log(`result: ${util.inspect(result.body)}`)
+
+      assert.equal(result.body.success, true)
+    })
+  })
+  */
+})
