e12296485e74220dc87ae85407b493fda8e84170
danielhumgon <danielhumgon@gmail.com>
2019-06-10 06:07:30 -0400

    added loggedwork changes

--- src/modules/logged-work/controller.js
+++ src/modules/logged-work/controller.js
@@ -1,5 +1,76 @@
-const Project = require('../../models/logged-work')
+
+const LoggedWork = require('../../models/logged-work')
+
+async function createLoggedWork (ctx) {
+  const loggedWork =   new LoggedWork(ctx.request.body.loggedWork) ;
+
+
+  try {
+    await loggedWork.save()
+  } catch (err) {
+    ctx.throw(422, err.message)
+  }
+
+  ctx.body = {
+    loggedWork: true,
+    
+  }
+}
+
+async function getLoggedWorks (ctx) {
+  const loggedWork = await LoggedWork.find({})
+  ctx.body = { loggedWork }
+}
+
+async function getLoggedWork (ctx, next) {
+  try {
+    
+    const loggedWork = await LoggedWork.findById(ctx.params.id)
+    if (!loggedWork) {
+      ctx.throw(404)
+    }
+
+    ctx.body = {
+        loggedWork
+    }
+  } catch (err) {
+    if (err === 404 || err.name === 'CastError') {
+      ctx.throw(404)
+    }
+
+    ctx.throw(500)
+  }
+
+  if (next) { return next() }
+}
+
+async function updateLoggedWork (ctx) {
+   const loggedWork = ctx.body.loggedWork
+   Object.assign(loggedWork, ctx.request.body.loggedWork);
+
+  await loggedWork.save()
+
+  ctx.body = {
+    loggedWork
+  }
+}
+
+async function deleteLoggedWork (ctx) {
+ 
+  const loggedWork = ctx.body.loggedWork;
+
+  await loggedWork.remove()
+
+  ctx.status = 200
+  ctx.body = {
+    success: true
+  }
+}
 
 module.exports = {
-
+    createLoggedWork,
+    getLoggedWorks,
+    getLoggedWork,
+    updateLoggedWork,
+    deleteLoggedWork
 }
--- src/modules/logged-work/router.js
+++ src/modules/logged-work/router.js
@@ -1,9 +1,48 @@
 const validator = require('../../middleware/validators')
-const project = require('./controller')
+const loggedWork = require('./controller')
 
-// export const baseUrl = '/users'
+
 module.exports.baseUrl = '/loggedwork'
 
 module.exports.routes = [
-
+  {
+    method: 'POST',
+    route: '/',
+    handlers: [
+        validator.ensureUser,
+        loggedWork.createLoggedWork
+    ]
+  },
+  {
+    method: 'GET',
+    route: '/',
+    handlers: [
+      loggedWork.getLoggedWorks
+    ]
+  },
+  {
+    method: 'GET',
+    route: '/:id',
+    handlers: [
+      loggedWork.getLoggedWork
+    ]
+  },
+  {
+    method: 'PUT',
+    route: '/:id',
+    handlers: [
+      validator.ensureUser,
+      loggedWork.getLoggedWork,
+      loggedWork.updateLoggedWork
+    ]
+  },
+  {
+    method: 'DELETE',
+    route: '/:id',
+    handlers: [
+      validator.ensureUser,
+      loggedWork.getLoggedWork,
+      loggedWork.deleteLoggedWork
+    ]
+  }
 ]
